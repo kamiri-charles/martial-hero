8d82271f0d4e651337c3c747765e5cea4163f4be
Charles Kamiri <charleskamiri6@gmail.com>
2023-09-25 11:46:21 +0300

    Polished controls

---
+++ src/modules/Controller/index.ts
@@ -0,0 +1,48 @@
+import Player from "../Player";
+import { player_states } from "../Player/player_states";
+
+export default class Controller {
+  player: Player;
+  keys_pressed: { [key: string]: boolean };
+
+  constructor(player: Player) {
+    this.player = player;
+    this.keys_pressed = {};
+
+    // Event listeners
+    window.addEventListener(
+      "keydown",
+      (e) => (this.keys_pressed[e.key] = true)
+    );
+    window.addEventListener("keyup", (e) => (this.keys_pressed[e.key] = false));
+  }
+
+  // Player controls
+  handle_input() {
+    if (!this.player.is_jumping) {
+      if (this.keys_pressed["ArrowRight"]) {
+        this.player.set_state(player_states.SPRINT_RIGHT);
+      } else if (this.keys_pressed["ArrowLeft"]) {
+        this.player.set_state(player_states.SPRINT_LEFT);
+      } else {
+        // If no movement keys are pressed, set the player to idle
+        if (this.player.on_ground()) {
+          if (this.player.state.includes("left")) {
+            this.player.set_state(player_states.IDLE_LEFT);
+          } else if (this.player.state.includes("right")) {
+            this.player.set_state(player_states.IDLE_RIGHT);
+          }
+        }
+      }
+    }
+
+    // Check for jump key separately
+    if (this.keys_pressed["ArrowUp"] && !this.player.is_jumping) {
+      if (this.player.state.includes("left")) {
+        this.player.set_state(player_states.JUMP_LEFT);
+      } else if (this.player.state.includes("right")) {
+        this.player.set_state(player_states.JUMP_RIGHT);
+      }
+    }
+  }
+}
--- src/modules/Game/index.ts
+++ src/modules/Game/index.ts
@@ -1,20 +1,17 @@
 import Player from "../Player";
-import { player_states } from "../Player/player_states";
+import Controller from "../Controller";
 
 export default class Game {
   canvas: HTMLCanvasElement;
   context: CanvasRenderingContext2D;
   player: Player;
-  keys_pressed: { [key: string]: boolean };
+  controller: Controller;
   
   constructor(canvas: HTMLCanvasElement, context: CanvasRenderingContext2D) {
     this.canvas = canvas;
     this.context = context;
     this.player = new Player(canvas);
-    this.keys_pressed = {};
-    
-    window.addEventListener("keydown", (e) => this.keys_pressed[e.key] = true);
-    window.addEventListener("keyup", (e) => this.keys_pressed[e.key] = false);
+    this.controller = new Controller(this.player);
   }
   
   // Main game loop
@@ -35,33 +32,13 @@ export default class Game {
       this.canvas.height
       );
       this.context.closePath();
-      
+
       // Player controls
-      if (!this.player.is_jumping) {
-        if (this.keys_pressed["ArrowRight"]) {
-          this.player.set_state(player_states.SPRINT_RIGHT);
-        } else if (this.keys_pressed["ArrowLeft"]) {
-          this.player.set_state(player_states.SPRINT_LEFT);
-        } else if (this.keys_pressed["ArrowUp"]) {
-          if (this.player.state.includes("left")) {
-            this.player.set_state(player_states.JUMP_LEFT);
-          } else if (this.player.state.includes("right")) {
-            this.player.set_state(player_states.JUMP_RIGHT);
-          }
-        } else {
-          if (this.player.on_ground()) {
-            if (this.player.state.includes("left")) {
-              this.player.set_state(player_states.IDLE_LEFT);
-            } else if (this.player.state.includes("right")) {
-              this.player.set_state(player_states.IDLE_RIGHT);
-            }
-          }
-        }
-      }
+      this.controller.handle_input();
       
       // Draw player
-      this.player.draw(this.context);
       this.player.update();
+      this.player.draw(this.context);
       
       requestAnimationFrame(() => this.run());
     }
